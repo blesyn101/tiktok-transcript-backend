0564177c6893db7d252e8d67d8859835bcbaea5a
Agharese Blessing <blessing4coding@gmail.com>
2026-04-01 11:57:54 +0100

    fix: add root route handler

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import transcriptRoutes from './routes/transcript.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import transcriptRoutes from "./routes/transcript.js";
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
@@ -10,11 +10,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.get('/api', (req, res) => {
-//     res.send("Transcript Api running");
-// });
-app.use('/api/transcript', transcriptRoutes);  
+app.get("/", (req, res) => {
+  res.send("Transcript Api running");
+});
+app.use("/api/transcript", transcriptRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
